f65d881307297d180c67985915d4ab11b7e4cec8
camilo1rp <camilo1rp@gmail.com>
2022-11-10 15:13:54 -0500

    change if statement for readability, created copy of list instead of sorting existing and creaed output message

--- find_pairs.py
+++ find_pairs.py
@@ -1,3 +1,6 @@
+from string import Template
+
+
 def pairs_finder(addends_list: list, target: int) -> list:
     """This function finds pairs of integers from a list that sum to a given value
     Args:
@@ -7,28 +10,31 @@ def pairs_finder(addends_list: list, target: int) -> list:
         A list of tuples with pairs of integers
     """
 
-    # sort list by using tims sort algorithm O(log n)
-    addends_list.sort()
+    # create a sort list by using tims sort algorithm O(log n)
+    addends_list_sorted = sorted(addends_list)
     addends = []
     add_pair = addends.append
     # loop thorough ordered list and find pairs
-    for n in addends_list:
+    for n in addends_list_sorted:
+        # find out if a pair number exists for n in list and
+        # since list is ordered, the last number must be greater than pair.
+        # if it is not, then is possible to discard it, and move to the previous one
         pair = target - n
-        # find out if pair for n exists in list and
-        # since list is ordered, the last element must be greater than pair.
-        # if it is not, then is possible to discard it
-        while  len(addends_list) > 1 and addends_list[-1] > pair:
-            addends_list.pop()
+        while  len(addends_list_sorted) > 1 and addends_list_sorted[-1] > pair:
+            addends_list_sorted.pop()
         else:
-            if pair == addends_list[-1]:
-                if pair != n:
-                    add_pair((n, pair))
-                    addends_list.pop()
+            if pair == addends_list_sorted[-1] and pair != n: # avoid adding the same number
+                add_pair((n, pair))
+                addends_list_sorted.pop()
     return addends
 
 
 if __name__ == '__main__':
     input_list = [1, 9, 5, 0, 20, -4, 12, 16, 7, 12]
     target = 12
-    pairs = pairs_finder(input_list.copy(), target)
-    print(f'the possible pairs for {target} from the list: {str(input_list)} are:\n{pairs}')
+    pairs = pairs_finder(input_list, target)
+    template_output = Template('\t $index), $pair1, $pair2')
+    print(f'the possible pairs for {target} from the list: {str(input_list)} are:')
+    # [print("\t",f'{index, pair[0], "and", pair[1]) for index, pair in enumerate(pairs, start=1)]
+    [print(template_output.substitute(index=index, pair1=pair[0], pair2=pair[1]))
+     for index, pair in enumerate(pairs, start=1)]
